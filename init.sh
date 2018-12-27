f45ba5033d4695bebfc8648ca18d5f9ce1240eca
Michael JIN <emj365@gmail.com>
2018-12-27 19:24:47 +0800

    add init.sh

---
+++ init.sh
@@ -0,0 +1,5 @@
+wget -O ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/honukai.zsh-theme https://raw.githubusercontent.com/oskarkrawczyk/honukai-iterm-zsh/master/honukai.zsh-theme
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
