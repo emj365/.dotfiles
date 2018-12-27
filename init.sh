febccf02c9a5e801a832221b16604b0f2ca8d382
Michael JIN <emj365@gmail.com>
2018-12-27 22:24:28 +0800

    optimize init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,8 @@
-wget -O ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/honukai.zsh-theme https://raw.githubusercontent.com/oskarkrawczyk/honukai-iterm-zsh/master/honukai.zsh-theme
+# ln -s $(pwd)/.gitconfig ~/
+# ln -s $(pwd)/.gitignore_global ~/
+
+mkdir -p ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes
+wget -O ${ZSH_CUSTOM:-~/.oh-my-zsh/customr}/themes/honukai.zsh-theme https://raw.githubusercontent.com/oskarkrawczyk/honukai-iterm-zsh/master/honukai.zsh-theme
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
